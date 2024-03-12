2650234ff2f00517021e68ebe764a16e2e9a9ac5
Samuel Grant <sgrant@fnal.gov>
2024-03-12 16:54:54 -0500

    Added scan conditions.

--- Analyses/PyMacros/CoincidenceConditions.py
+++ Analyses/PyMacros/CoincidenceConditions.py
@@ -15,6 +15,13 @@
 
 coincidenceConditions_ = { 
 
+    # "original" : {
+    #     "PEthreshold" : 20 
+    #     ,"nLayers" : 3
+    #     ,"minSlope" : -11
+    #     ,"maxSlope" : 11
+    # },
+
     "default" : {
         "PEthreshold" : 1 
         ,"nLayers" : 1
@@ -23,18 +30,79 @@ coincidenceConditions_ = {
     },
 
     "ana1" : {
-        "PEthreshold" : 55
+        "PEthreshold" : 10
         ,"nLayers" : 3
         ,"minSlope" : -11
         ,"maxSlope" : 11
     },
 
-    "original" : {
-        "PEthreshold" : 20 
-        ,"nLayers" : 3
-        ,"minSlope" : -11
-        ,"maxSlope" : 11
-    },
+    # Inefficient, but I think in this context it's perfectly OK. 
 
+    "10PEs3Layers": {
+        "PEthreshold": 10,
+        "nLayers": 3,
+        "minSlope": -11,
+        "maxSlope": 11
+    },
+    "11PEs3Layers": {
+        "PEthreshold": 11,
+        "nLayers": 3,
+        "minSlope": -11,
+        "maxSlope": 11
+    },
+    "12PEs3Layers": {
+        "PEthreshold": 12,
+        "nLayers": 3,
+        "minSlope": -11,
+        "maxSlope": 11
+    },
+    "13PEs3Layers": {
+        "PEthreshold": 13,
+        "nLayers": 3,
+        "minSlope": -11,
+        "maxSlope": 11
+    },
+    "14PEs3Layers": {
+        "PEthreshold": 14,
+        "nLayers": 3,
+        "minSlope": -11,
+        "maxSlope": 11
+    },
+    "15PEs3Layers": {
+        "PEthreshold": 15,
+        "nLayers": 3,
+        "minSlope": -11,
+        "maxSlope": 11
+    },
+    "16PEs3Layers": {
+        "PEthreshold": 16,
+        "nLayers": 3,
+        "minSlope": -11,
+        "maxSlope": 11
+    },
+    "17PEs3Layers": {
+        "PEthreshold": 17,
+        "nLayers": 3,
+        "minSlope": -11,
+        "maxSlope": 11
+    },
+    "18PEs3Layers": {
+        "PEthreshold": 18,
+        "nLayers": 3,
+        "minSlope": -11,
+        "maxSlope": 11
+    },
+    "19PEs3Layers": {
+        "PEthreshold": 19,
+        "nLayers": 3,
+        "minSlope": -11,
+        "maxSlope": 11
+    },
+    "20PEs3Layers": {
+        "PEthreshold": 20,
+        "nLayers": 3,
+        "minSlope": -11,
+        "maxSlope": 11
+    }
 
 }

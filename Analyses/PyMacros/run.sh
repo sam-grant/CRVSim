443941bf070feee8a17e4498015391263c188432
Samuel Grant <sgrant@fnal.gov>
2024-03-15 15:58:39 -0500

    Updated porallelisation scripts.

--- Analyses/PyMacros/run.sh
+++ Analyses/PyMacros/run.sh
@@ -1,4 +1,41 @@
-filterCondition=$1
-finName=$2
-python CoincidenceFinder.py $filterCondition $finName
-sleep 1
+FILE=$1
+PARTICLE=$2
+PE=$3 
+
+COINC="${PE}PEs2Layers"
+
+# FILENAME=$(basename "$FILE" ".root") 
+
+ID=${FILE%%.root}
+ID=${ID##*.}
+
+LOGDIR="../Logs/$ID"
+
+if [ -d $LOGDIR ]; then
+    # rm $LOGDIR/*.log 
+    echo "Using log directory ${LOGDIR}"
+else 
+    mkdir $LOGDIR
+    # echo "Created log directory ${LOGDIR}"
+fi
+
+LOGFILE="${LOGDIR}/${PARTICLE}_${COINC}.log"
+
+if [ -f $LOGFILE ]; then
+    rm $LOGFILE && touch $LOGFILE
+else 
+    touch $LOGFILE
+fi
+
+echo "Log: ${LOGFILE}"
+python Analyse.py $FILE $PARTICLE $COINC >> $LOGFILE 
+
+# if [ -f $LOGFILE ]; then
+#     # rm $LOGFILE && touch $LOGFILE
+#     echo "Skipping" 
+# else 
+#     touch $LOGFILE
+# fi
+
+# echo "Log: ${LOGFILE}"
+# python Analyse.py $FILE $PARTICLE $COINC >> $LOGFILE 
--- Analyses/PyMacros/submit.sh
+++ Analyses/PyMacros/submit.sh
@@ -1,9 +1,22 @@
 
-nCores=3
-for filterCondition in "no_filter" "one_coincidence_per_sector" "one_coincidence_per_trigger_sector"; do 
-    for file in `mu2eDatasetFileList nts.mu2e.CosmicCRYExtractedTrk.MDC2020z1_best_v1_1_std_v04_01_00.tka`; do
-        echo $filterCondition $file
-        sleep 1
-    done | xargs -P $nCores -I {} bash -c ". run.sh {}"
-    sleep 1
-done
+# nCores=3
+# for filterCondition in "no_filter" "one_coincidence_per_sector" "one_coincidence_per_trigger_sector"; do 
+#     for file in `mu2eDatasetFileList nts.mu2e.CosmicCRYExtractedTrk.MDC2020z1_best_v1_1_std_v04_01_00.tka`; do
+#         echo $filterCondition $file
+#         sleep 1
+#     done | xargs -P $nCores -I {} bash -c ". run.sh {}"
+#     sleep 1
+# done
+
+NCORES=4
+for FILE in $(cat ../Txt/Lists/ReprocessedFiles.txt); do 
+    for PARTICLE in "all" "muons" "non_muons"; do
+        # for PE in 10 12 14 16 18 20; do
+        # for PE in 10; do # 22 24 26 28 30 32; do
+        for PE in 10; do #  12 14 16 18 20 22 24 26 28 30 32; do
+        
+            echo $FILE $PARTICLE $PE 
+            # break 3 # UNCOMMENT FOR TESTING
+        done 
+    done
+done | xargs -P $NCORES -I {} bash -c ". run.sh {}"

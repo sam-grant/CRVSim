0008210ef35aff4c2820935ab21c16f8863d1848
sam-grant <samuel.grant94@gmail.com>
2024-08-14 18:43:17 +0000

    Minor updates.

--- Analyses/Scripts/make_dir.sh
+++ Analyses/Scripts/make_dir.sh
@@ -1,16 +1,12 @@
-for FILE in $(cat ../Txt/Lists/MDC2020ae.txt); do 
+for FILE in $(cat ../Txt/FileLists/MDC2020ae.txt); do 
 
     ID=${FILE%%.root}
     ID=${ID##*.}
-
-    # mkdir ../results/$ID
-    #mkdir failures_concise/$ID
-    #mkdir failures_verbose/$ID
-    # mkdir ../Txt/reprocessed/PEsPerLayer/$ID
-    #mkdir -p ../Txt/MDC2020ae/results/$ID
-    #mkdir -p ../Txt/MDC2020ae/failures_concise/$ID
-    #mkdir -p ../Txt/MDC2020ae/failures_verbose/$ID
-    #mkdir -p ../Txt/MDC2020ae/failures_ntuple/$ID
+    
+    mkdir -p ../Txt/MDC2020ae/results/$ID
+    mkdir -p ../Txt/MDC2020ae/failures_concise/$ID
+    mkdir -p ../Txt/MDC2020ae/failures_verbose/$ID
+    mkdir -p ../Txt/MDC2020ae/failures_ntuple/$ID
     mkdir -p  ../Txt/MDC2020ae/PEsPerLayer/$ID
 
 done

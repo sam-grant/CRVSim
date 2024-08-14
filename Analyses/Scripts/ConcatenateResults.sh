19d268c21c11bc4d7a35880dc14d31d7f049797d
sam-grant <samuel.grant94@gmail.com>
2024-08-14 18:43:57 +0000

    Minor updates.

--- Analyses/Scripts/ConcatenateResults.sh
+++ Analyses/Scripts/ConcatenateResults.sh
@@ -5,7 +5,7 @@ recon="MDC2020ae" # "original"
 particle_=("all" "muons" "non_muons")
 coincidenceFilter="one_coincidence_per_trigger_sector"
 
-PEs_=($(seq 10.0 2.5 130.0))
+PEs_=($(seq 10.0 5.0 130.0))
 
 # if [ -f 
 # dir
@@ -17,7 +17,7 @@ PEs_=($(seq 10.0 2.5 130.0))
 
 # for FILE in $(cat ../Txt/Lists/MDC2020ae.txt); do 
 # for PARTICLE in "all"; do 
-for particle in "all"; do # "muons" "non_muons"; do
+for particle in "all" "muons" "non_muons"; do
     for layer in 2 3; do
         for PE in ${PEs_[@]}; do 
         
@@ -26,8 +26,9 @@ for particle in "all"; do # "muons" "non_muons"; do
             
             coincidenceConditions="${PE}PEs${layer}Layers"
 
-            # config="${particle}_${coincidenceConditions}_${coincidenceFilter}"
-            config="${coincidenceConditions}_${coincidenceFilter}"
+            config="${particle}_${coincidenceConditions}_${coincidenceFilter}"
+            
+            # config="${coincidenceConditions}_${coincidenceFilter}"
 
             # i="failures_ntuple"
 
@@ -59,61 +60,66 @@ for particle in "all"; do # "muons" "non_muons"; do
 
             i="results"
 
-            fout="../Txt/${recon}/concatenated/${i}_${config}.csv"
+            fout="../Txt/${recon}/concatenated2/${i}_${config}.csv"
 
             if [ ! -f $fout ]; then
-            #     rm $fout && touch $fout
-            # else 
+                rm $fout && touch $fout
+            else 
                 touch $fout
-            
-                header=$(awk 'NR==1' $(ls ../Txt/${recon}/${i}/001205_00000000/results_${config}.csv -1))
-    
-                echo "Start run/subrun, ${header}" >> $fout 
-    
-                for fin in $(ls ../Txt/${recon}/${i}/0*/${i}_${config}.csv | sort -V); do
-                    # id=${fin%%*/${i}}
-                    # id=${id##*/${i}}
-                    id=$(echo "$fin" | awk -F'/' '{print $(NF-1)}')
-    
-                    results=$(awk 'NR==2' "$fin")
-                    echo "${id}, ${results}" >> $fout
-                done
-    
-                echo "---> Written ${fout}"
-
             fi
+                
+            header=$(awk 'NR==1' $(ls ../Txt/${recon}/${i}/001205_00000000/results_${config}.csv -1))
+
+            echo "Start run/subrun, ${header}" >> $fout 
+
+            for fin in $(ls ../Txt/${recon}/${i}/0*/${i}_${config}.csv | sort -V); do
+                # id=${fin%%*/${i}}
+                # id=${id##*/${i}}
+                id=$(echo "$fin" | awk -F'/' '{print $(NF-1)}')
+
+                results=$(awk 'NR==2' "$fin")
+                echo "${id}, ${results}" >> $fout
+            done
+
+            echo "---> Written ${fout}"
+
 
             i="failures_concise"
 
-            fout="../Txt/${recon}/concatenated/${i}_${config}.csv"
+            fout="../Txt/${recon}/concatenated2/${i}_${config}.csv"
 
             if [ ! -f $fout ]; then
-            #     rm $fout && touch $fout
-            # else 
+                rm $fout && touch $fout
+            else 
                 touch $fout
-            
-                header=$(awk 'NR==1' $(ls ../Txt/${recon}/${i}/001205_00000000/${i}_${config}.csv -1))
-                echo "Start run/subrun, ${header}" >> $fout 
-    
-                for fin in $(ls ../Txt/${recon}/${i}/0*/${i}_${config}.csv | sort -V); do
-                    # id=${fin%%${i}}
-                    # id=${id##*${i}}
-                    id=$(echo "$fin" | awk -F'/' '{print $(NF-1)}')
-                    awk 'NR>1 {print "'"$id"', " $0}' "$fin" >> "$fout"
-                done
-    
-                echo "---> Written ${fout}"
-
             fi
+            
+            header=$(awk 'NR==1' $(ls ../Txt/${recon}/${i}/001205_00000000/${i}_${config}.csv -1))
+            echo "Start run/subrun, ${header}" >> $fout 
 
-            # i="failures_verbose"
-            # fout="../Txt/${recon}/concatenated/${i}_${config}.csv"
+            for fin in $(ls ../Txt/${recon}/${i}/0*/${i}_${config}.csv | sort -V); do
+                # id=${fin%%${i}}
+                # id=${id##*${i}}
+                id=$(echo "$fin" | awk -F'/' '{print $(NF-1)}')
+                awk 'NR>1 {print "'"$id"', " $0}' "$fin" >> "$fout"
+            done
 
-            # for fin in $(ls ../Txt/${recon}/${i}/0*/${i}_${config}.csv | sort -V); do
-            #     cat $fin >> $fout
-            # done
+            echo "---> Written ${fout}"
+        
+            i="failures_verbose"
+            fout="../Txt/${recon}/concatenated2/${i}_${config}.csv"
 
-            # echo "---> Written ${fout}"
+            if [ ! -f $fout ]; then
+                rm $fout && touch $fout
+            else 
+                touch $fout
+            fi
+            
+            for fin in $(ls ../Txt/${recon}/${i}/0*/${i}_${config}.csv | sort -V); do
+                cat $fin >> $fout
+            done
+
+            echo "---> Written ${fout}"
 
         done
 

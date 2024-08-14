06a0202e6d51ae61722009315cad1cb687620be3
sam-grant <samuel.grant94@gmail.com>
2024-08-14 18:52:45 +0000

    Updated plotting code.

--- Analyses/PyMacros/ModuleEfficiencyScan.py
+++ Analyses/PyMacros/ModuleEfficiencyScan.py
@@ -6,9 +6,71 @@ Plot the results of the PE threshold scan
 import sys
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
 import Utils as ut
 import CoincidenceConditions as cc
+
+# May not be strictly accurate.
+def GetRatioUncertainty(dataAll):
+  n_failures = np.sum(dataAll[" Failures"])
+  n_total = np.sum(dataAll[" Total"])
+  ratio = n_failures / n_total
+  se_ratio = np.sqrt(ratio * (1 - ratio) / n_total)
+  return se_ratio
+
+def PlotGraphOverlay(graphs_, title=None, xlabel=None, ylabel=None, ymin=1, ymax=-1, labels_=[], fout="scatter.png", effLine=False, log=False, includeBlack=False, NDPI=300):
+    
+    # Create figure and axes
+    fig, ax = plt.subplots()
+
+    # Iterate over each pair of xy lists
+    for i, (label, data_) in enumerate(graphs_.items()):
+
+        x = data_[0]
+        xerr = data_[1]
+        y = data_[2]
+        yerr = data_[3]
+
+         # Plot scatter with error bars
+        if len(xerr)==0: xerr = [0] * len(x) # Sometimes we only use yerr
+        if len(yerr)==0: yerr = [0] * len(y) # Sometimes we only use yerr
+
+        colour = ut.colours[i+1]
+        if includeBlack: colour = ut.colours[i]
+
+        ax.errorbar(x, y, xerr=xerr, yerr=yerr, fmt='o', color=colour, markersize=4, ecolor=colour, capsize=2, elinewidth=1, linestyle='-',label=label)
+
+    if log: 
+        ax.set_yscale("log")
+
+    # Set title, xlabel, and ylabel
+    ax.set_title(title, fontsize=15, pad=10)
+    ax.set_xlabel(xlabel, fontsize=13, labelpad=10)
+    ax.set_ylabel(ylabel, fontsize=13, labelpad=10)
+
+    # Set font size of tick labels on x and y axes
+    ax.tick_params(axis='x', labelsize=13)  
+    ax.tick_params(axis='y', labelsize=13)  
+
+    if (ymax > ymin):
+        ax.set_ylim(ymin, ymax)
+
+    # Add a line at 99.99% efficiency
+    if effLine:
+        ax.text(100, 1.2e-4, "99.99% efficiency", color="gray") #, transform=ax.transAxes, ha='right')
+        ax.axhline(y=1e-4, color='gray', linestyle='--')
+
+    ax.legend(loc="best", frameon=False, fontsize=13) # , markerscale=5)
+
+    # Save the figure
+    plt.savefig(fout, dpi=NDPI, bbox_inches="tight")
+    print("---> Written", fout)
+
+    # Clear memory
+    plt.close()
+
+    return
 
 # ------------------------------------------------
 #                       Run
@@ -17,8 +79,10 @@ import CoincidenceConditions as cc
 def Run(): 
 
     dataset = "MDC2020ae"
-    PEs_ = list(range(10, 151, 2)) 
-    layers_ = [3] # 2, 3] 
+    # PEs_ = list(range(10, 151, 2)) 
+    # PEs_ = np.arange(10, 132.5, 2.5)
+    PEs_ = np.arange(10.0, 135.0, 5.0)
+    layers_ = [2, 3] 
 
     # Create new dictionaries with independent keys and values
     # ineff_ = {key: value.copy() for key, value in data_.items()}
@@ -31,11 +95,11 @@ def Run():
     wb_ineff = dataWB["Observed 3/4 inefficiency"]
     wb_ineff_err = dataWB["Error bars, observed 3/4 inefficiency"]
 
-    shared_thresholds = []
-    shared_wb_ineff = []
-    shared_wb_ineff_err = []
+    # shared_thresholds = []
+    # shared_wb_ineff = []
+    # shared_wb_ineff_err = []
 
-    print(wb_thresholds)
+    # print(wb_thresholds)
 
     for layer in layers_: 
 
@@ -53,36 +117,7 @@ def Run():
             ,"Non-muons" : [ PEs_, [], [], [] ]     
         }
 
-        # count = 0
-        count = 0
-
         for PE in PEs_:
-
-            if PE not in np.array([wb_thresholds]): 
-                continue
-            else:
-                print(PE)
-                shared_wb_ineff.append(wb_ineff[count])
-                shared_wb_ineff_err.append(wb_ineff_err[count])
-                shared_thresholds.append(PE)
-                count+=1
-
-            # print(PE)
-            # print(wb_thresholds[count])
-            # print()
-            # count += 1
-            # continue
-
-            # if (PE != wb_thresholds[count]): 
-            #     # count+=1
-            #     continue
-            # else:
-            #     print(PE)
-            #     print(wb_thresholds[count])
-            #     print()
-            #     count+=1
-
-            # continue
                 
             finNameAll = f"../Txt/{dataset}/concatenated/results_all_{PE}PEs{layer}Layers_one_coincidence_per_trigger_sector.csv"
             finNameMuons = f"../Txt/{dataset}/concatenated/results_muons_{PE}PEs{layer}Layers_one_coincidence_per_trigger_sector.csv"
@@ -93,31 +128,47 @@ def Run():
             dataNonMuons = pd.read_csv(finNameNonMuons) 
 
             ineff_["All"][2].append(np.sum(dataAll[" Failures"])/np.sum(dataAll[" Total"]))
-            ineff_["Muons"][2].append(np.sum(dataMuons[" Failures"])/np.sum(dataMuons[" Total"])) 
-            ineff_["Non-muons"][2].append(np.sum(dataNonMuons[" Failures"])/np.sum(dataNonMuons[" Total"])) 
+            ineff_["All"][3].append(GetRatioUncertainty(dataAll))
 
+            ineff_["Muons"][2].append(np.sum(dataMuons[" Failures"])/np.sum(dataMuons[" Total"]))
+            ineff_["Muons"][3].append(GetRatioUncertainty(dataMuons))
+
+            ineff_["Non-muons"][2].append(np.sum(dataNonMuons[" Failures"])/np.sum(dataNonMuons[" Total"]))
+            ineff_["Non-muons"][3].append(GetRatioUncertainty(dataNonMuons))
+    
             nfailures_["All"][2].append(np.sum(dataAll[" Failures"]))
             nfailures_["Muons"][2].append(np.sum(dataMuons[" Failures"]))
             nfailures_["Non-muons"][2].append(np.sum(dataNonMuons[" Failures"]))
-
-            # count += 1
-
-        # ut.PlotGraphOverlay2(nfailures_, title=f"{layer}/4 layers", xlabel="PEs per layer threshold", ylabel="Failures", fout=f"../Images/{dataset}/ThresholdScan/gr_log_overlay_nfailures_{layer}layers.png", log=True)
-        # ut.PlotGraphOverlay2(ineff_, title=f"{layer}/4 layers", xlabel="PEs per layer threshold", ylabel="Inefficiency", fout=f"../Images/{dataset}/ThresholdScan/gr_log_overlay_ineff_{layer}layers.png", log=True)
-        # ut.PlotGraphOverlay2(nfailures_, title=f"{layer}/4 layers", xlabel="PEs per layer threshold", ylabel="Failures", fout=f"../Images/{dataset}/ThresholdScan/gr_overlay_nfailures_{layer}layers.png", log=False)
-        # ut.PlotGraphOverlay2(ineff_, title=f"{layer}/4 layers", xlabel="PEs per layer threshold", ylabel="Inefficiency", fout=f"../Images/{dataset}/ThresholdScan/gr_overlay_ineff_{layer}layers.png", log=False)
         
-        # if (layer==3):
-        print(ineff_["All"][2])
-        print(wb_ineff)
+        print("PEs:", PEs_)
+        print("Failures (muons):", nfailures_["Muons"][2])
+        
+        PlotGraphOverlay(nfailures_, xlabel="Threshold [PEs/layer]", ylabel=f"Failures ({layer}/4 layers)", fout=f"../Images/{dataset}/ThresholdScan/gr_log_overlay_nfailures_{layer}layers.png", log=True)
+        PlotGraphOverlay(nfailures_, xlabel="Threshold [PEs/layer]", ylabel=f"Failures ({layer}/4 layers)", fout=f"../Images/{dataset}/ThresholdScan/gr_overlay_nfailures_{layer}layers.png", log=False)
+        
+        PlotGraphOverlay(ineff_, xlabel="Threshold [PEs/layer]", ymin=5e-7, ymax=1e0, ylabel=f"{layer}/4 inefficiency", fout=f"../Images/{dataset}/ThresholdScan/gr_overlay_ineff_{layer}layers.png", log=False)
+        PlotGraphOverlay(ineff_, xlabel="Threshold [PEs/layer]", ymin=5e-7, ymax=1e0, ylabel=f"{layer}/4 inefficiency", fout=f"../Images/{dataset}/ThresholdScan/gr_log_overlay_ineff_{layer}layers.png", log=True)
+        
+        # Comparison with data
+        if layer != 3: # Tyler only has 3/4 layers
+            continue 
+
         data_ = { 
             # What's the deal with this structure? 
             # x, xerr, y, yerr
-            "Sim" : [ shared_thresholds, [], ineff_["All"][2], [] ] 
-            ,"Data" : [ shared_thresholds, [], shared_wb_ineff, shared_wb_ineff_err]     
+            "Data" : [ PEs_, np.array([]), wb_ineff, wb_ineff_err]   
+            ,"Sim (all)" : [ PEs_, np.array([]), ineff_["All"][2], ineff_["All"][3] ]
+            ,"Sim (muons)" : [ PEs_, np.array([]), ineff_["Muons"][2], ineff_["Muons"][3] ] 
+            ,"Sim (non-muons)" : [ PEs_, np.array([]), ineff_["Non-muons"][2], ineff_["Non-muons"][3] ] 
+              
         }
-        ut.PlotGraphOverlay2(data_, title=f"3/4 layers", xlabel="PEs per layer threshold", ylabel="Inefficiency", fout=f"../Images/{dataset}/ThresholdScan/gr_data_sim_overlay_ineff_3layers.png", log=True)
 
+        # print(PEs_)
+        # print(ineff_["Muons"][2])
+        
+        PlotGraphOverlay(data_, xlabel="Threshold [PEs/layer]", ymin=5e-7, ymax=1e0, ylabel=f"{layer}/4 inefficiency", fout=f"../Images/{dataset}/ThresholdScan/gr_data_sim_overlay_ineff_{layer}layers.png", log=False, effLine=True, includeBlack=True)
+        PlotGraphOverlay(data_, xlabel="Threshold [PEs/layer]", ymin=5e-7, ymax=1e0, ylabel=f"{layer}/4 inefficiency", fout=f"../Images/{dataset}/ThresholdScan/gr_log_data_sim_overlay_ineff_{layer}layers.png", log=True, effLine=True, includeBlack=True)
+        
     return
 
 # ------------------------------------------------
